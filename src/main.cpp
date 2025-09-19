42b287520865066ad03b65953605e28bbe11b22c
Roman Yakovlev <yakovlev.roman@gmail.com>
2025-09-19 21:42:00 +0300

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
-// #include <ESP8266WiFi.h>
-// #include <ESPAsyncTCP.h>
-// #include <ESPAsyncWebServer.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-// #include <LittleFS.h>
+#include <LittleFS.h>
 
-// #include <prefs.h>
+#include <prefs.h>
 #include <numitron.h>
-// #include <reqHandlers.h>
+#include <reqHandlers.h>
 
 #define LED_PIN 12
 
@@ -24,9 +24,9 @@ typedef enum
   CLOCK_MODE_DATE
 } ClockMode;
 
-// static AsyncWebServer server(80);
+static AsyncWebServer server(80);
 
-// static ClockMode clockMode = CLOCK_MODE_TIME;
+static ClockMode clockMode = CLOCK_MODE_TIME;
 
 void displayTime();
 
@@ -36,47 +36,48 @@ void setup()
 {
   Serial.begin(115200);
 
-  // numitron_init();
+  numitron_init();
+  numitron_setBrightness(64);
 
-  // LittleFS.begin();
-  // loadPreferences(prefs);
+  LittleFS.begin();
+  loadPreferences(prefs);
 
-  // if (prefs.wifiMode == WIFI_AP)
-  // {
-  //   WiFi.mode(WIFI_AP);
-  //   WiFi.softAP(prefs.hostname, prefs.password);
-  //   Serial.println("WiFi Access Point started.");
-  // }
-  // else
-  // {
-  //   WiFi.mode(WIFI_STA);
-  //   WiFi.begin(prefs.ssid, prefs.password);
-  //   Serial.println("WiFi Station mode started.");
+  if (prefs.wifiMode == WIFI_AP)
+  {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(prefs.hostname, prefs.password);
+    Serial.println("WiFi Access Point started.");
+  }
+  else
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(prefs.ssid, prefs.password);
+    Serial.println("WiFi Station mode started.");
 
-  //   if (WiFi.waitForConnectResult(WIFI_CONNECTION_TIMEOUT) != WL_CONNECTED)
-  //   {
-  //     Serial.println("Couldn't connect to wifi network");
-  //     WiFi.mode(WIFI_AP);
-  //     WiFi.softAP(prefs.ssid, prefs.password);
-  //   }
-  //   else
-  //   {
-  //     Serial.println("\nWiFi connected.");
-  //   }
-  // }
+    if (WiFi.waitForConnectResult(WIFI_CONNECTION_TIMEOUT) != WL_CONNECTED)
+    {
+      Serial.println("Couldn't connect to wifi network");
+      WiFi.mode(WIFI_AP);
+      WiFi.softAP(prefs.ssid, prefs.password);
+    }
+    else
+    {
+      Serial.println("\nWiFi connected.");
+    }
+  }
 
-  // configTzTime(prefs.timezone, "pool.ntp.org", "time.nist.gov");
+  configTzTime(prefs.timezone, "pool.ntp.org", "time.nist.gov");
 
-  // server.on("/", HTTP_GET, reqRoot);
-  // server.serveStatic("/index.html", LittleFS, "/index.html");
+  server.on("/", HTTP_GET, reqRoot);
+  server.serveStatic("/index.html", LittleFS, "/index.html");
 
-  // server.on("/get-network-settings", HTTP_GET, reqGetNetworkSettings);
-  // server.on("/save-network-settings", HTTP_POST, reqSaveNetworkSettings);
+  server.on("/get-network-settings", HTTP_GET, reqGetNetworkSettings);
+  server.on("/save-network-settings", HTTP_POST, reqSaveNetworkSettings);
 
-  // server.on("/get-time", HTTP_GET, reqGetTime);
-  // server.on("/save-time", HTTP_POST, reqSaveTime);
+  server.on("/get-time", HTTP_GET, reqGetTime);
+  server.on("/save-time", HTTP_POST, reqSaveTime);
 
-  // server.begin();
+  server.begin();
 }
 
 tm tmInfo;
@@ -84,23 +85,19 @@ uint8_t digs[NUMITRON_DIGIT_COUNT];
 
 void loop()
 {
-  // switch (clockMode)
-  // {
-  // case CLOCK_MODE_TIME:
-  //   displayTime();
-  //   break;
+  switch (clockMode)
+  {
+  case CLOCK_MODE_TIME:
+    displayTime();
+    break;
   
-  // case CLOCK_MODE_DATE:
-  //   displayDate();
-  //   break; 
+  case CLOCK_MODE_DATE:
+    displayDate();
+    break; 
 
-  // default:
-  //   break;
-  // }
-
-  // numitron_display(digs);
-  Serial.println("Hello, World!");
-  delay(1000);
+  default:
+    break;
+  }
 }
 
 void displayTime()
